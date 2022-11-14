a40b410fad8afe4b4906af1889c82007efeb9de0
Roberto Madinyá <roberto.madinya@ioet.com>
2022-11-13 20:09:51 -0500

    render template index (#2)

--- app/routers/index.py
+++ app/routers/index.py
@@ -1,11 +1,26 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from app.ports.input import ClientInputPort
+templates = Jinja2Templates('app/templates')
 
 index = APIRouter()
+MENU = [
+    {"description": "Clients", "url": ""},
+    {"description": "Contacts", "url": ""},
+    {"description": "Projects", "url": ""},
+    {"description": "Roles", "url": ""},
+]
+
+@index.get("/view/home", include_in_schema=False)
+async def get(request: Request):
+    result = [ item.__dict__ for item in ClientInputPort.get_all()]
+    return templates.TemplateResponse('index.html', {'request': request,
+                                                     'menus': MENU,
+                                                     'clients': result})
 
 
-@index.get("/")
+
+@index.get("/", include_in_schema=False)
 async def get(request: Request):
     return {"app": "clients app",
             "version": "0.1",
